4bd890c264891f6a2d882155ecd983195ef524c4
Dykim991222 <alfine122299@gmail.com>
2025-02-13 11:12:21 +0900

    Supabase Url, Api key

--- config/supabase.js
+++ config/supabase.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  // .env 파일 로드
+
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
@@ -6,3 +8,6 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey ? "✅ 키 로드 성공" : "❌ 키 로드 실패");
