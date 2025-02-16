321e6a7820d2de3107b5d3984595a9960c0a9f0b
soheeGit <63wlsthgml@gmail.com>
2025-02-16 15:25:46 +0900

    경로에 파라미터 추가

--- routers/feed_coment.js
+++ routers/feed_coment.js
@@ -9,7 +9,7 @@ const { isLoggedIn } = require("../middlewares");
 
 const router = express.Router();
 
-router.get("/getFeed", isLoggedIn, getFeed);
+router.get("/getFeed/:id", isLoggedIn, getFeed);
 router.get("/getComment", isLoggedIn, getComment);
 router.post("/addComment", isLoggedIn, addComment);
 router.post("/addCommetChat", isLoggedIn, addCommetChat);
